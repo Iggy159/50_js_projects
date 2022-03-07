917b40f4468a2fa955d2d064ecde4f5964cfcfbc
Iggy159 <georgebazhenov27@gmail.com>
2022-03-07 16:14:42 +0300

    22 drawing app

---
+++ 22_drawing_app/app.js
@@ -0,0 +1,82 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+const clearButton = document.getElementById('clear')
+const incButton = document.getElementById('increase')
+const decButton = document.getElementById('decrease')
+let colorLine = document.getElementById('color')
+
+
+
+let size = 20
+let color = colorLine
+let isPressed = false
+let x, y
+
+
+clearButton.addEventListener('click', clearCanvas)
+incButton.addEventListener('click', incLine)
+decButton.addEventListener('click', decLine)
+
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true
+
+    x = e.offsetX
+    y = e.offsetY
+})
+
+canvas.addEventListener('mouseup', (e) => {
+    isPressed = false
+
+    x = undefined
+    y = undefined
+})
+
+canvas.addEventListener('mousemove', (e) => {
+    if(isPressed) {
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+
+        drawCircle(x2, y2)
+        drawLine(x, y, x2, y2)
+
+        x = x2
+        y = y2
+    }
+})
+
+function drawCircle(x, y) {
+    ctx.beginPath()
+    ctx.arc(x, y, size, 0, Math.PI * 2)
+    ctx.fillStyle = color
+    ctx.fill()
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath()
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.strokeStyle = color
+    ctx.lineWidth = size * 2
+    ctx.stroke()
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function incLine() {
+    return size += 5
+}
+incLine()
+
+function decLine() {
+    return size -= 5
+}
+decLine()
+
+window.addEventListener('resize', resizeCanvas)
+function resizeCanvas () {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+}
+resizeCanvas()

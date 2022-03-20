b40bd93cb95cfae3ee2fba0e5422b07041433547
Egor <georgebazhenov27@gmail.com>
2022-03-20 18:58:19 +0300

    hoverboard

--- 36_hoverboard/app.js
+++ 36_hoverboard/app.js
@@ -1,2 +1,29 @@
 const container = document.getElementById('container')
-const colors = []
+const colors = ['#DAF7A6', '#FFC300', '#FF5733', '#C70039', '#900C3F', '#581845']
+
+const SQUARES = 448
+
+for(let i = 0; i < SQUARES; i++) {
+    const square  = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseout', () => removeColor(square))
+
+    container.appendChild(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 20px ${color}`
+}
+
+function removeColor(element) {
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}

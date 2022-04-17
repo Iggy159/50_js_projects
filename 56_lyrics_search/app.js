81e83be9419813a01d368967bf767360b3832147
Iggy159 <georgebazhenov27@gmail.com>
2022-04-17 23:53:15 +0300

    lyrics search

---
+++ 56_lyrics_search/app.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const form = document.getElementById('form')
+const form = document.getElementById('form')
+
+
+const apiURL = 'https://api.lyrics.ovh'

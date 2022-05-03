400df47266b06dc6e1db5c579c53f52a5d91f4bb
Iggy159 <georgebazhenov27@gmail.com>
2022-05-03 23:09:08 +0300

    lyrics app

--- 56_lyrics_search/app.js
+++ 56_lyrics_search/app.js
@@ -14,21 +14,51 @@ async function searchSongs(term) {
 }
 
 function showData(data) {
-    
+
+    result.innerHTML = `
+    <ul class="songs">
+        ${data.data.map(song => ` <li>
+        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+        </li>`)
+        .join('')
+    }
+    </ul>
+    `
+
+    if (data.prev || data.next) {
+        more.innerHTML = `
+          ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>` : ''}
+          ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>` : ''}`
+      } else {
+        more.innerHTML = '';
+      }
 }
 
-function showData(data) {
-    
+async function getMoreSongs(url) {
+    const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const data = await res.json();
+
+    showData(data);
 }
 
 
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
 
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
 
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>`
+
+    more.innerHTML = ''
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const searchTerm = serch.value.trim()
+    const searchTerm = search.value.trim()
 
     if(!searchTerm) {
         alert('Please type in a search term')
@@ -38,15 +68,13 @@ form.addEventListener('submit', (e) => {
     
 })
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
+result.addEventListener('click', e => {
+    const clickEl = e.target
 
-    const searchTerm = serch.value.trim()
+    if(clickEl.tagName === 'BUTTON') {
+        const artist = clickEl.getAttribute('data-artist')
+        const songTitle = clickEl.getAttribute('data-songtitle')
 
-    if(!searchTerm) {
-        alert('Please type in a search term')
-    } else {
-        searchSongs(searchTerm)
-    }
-    
+        getLyrics(artist, songTitle)
+    } 
 })
